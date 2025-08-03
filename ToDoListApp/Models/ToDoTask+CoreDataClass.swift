001b74dd3b09905da3dc193469471de02d572058
fedichv <fedichev06@bk.ru>
2025-08-03 16:53:47 +0300

    generate NSManagedObject subclass for Task

---
+++ ToDoListApp/Models/ToDoTask+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoTask+CoreDataClass.swift
+//  ToDoListApp
+//
+//  Created by Владимир Федичев on 8/3/25.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ToDoTask)
+public class ToDoTask: NSManagedObject {
+
+}
---
+++ ToDoListApp/Models/ToDoTask+CoreDataProperties.swift
@@ -0,0 +1,28 @@
+//
+//  ToDoTask+CoreDataProperties.swift
+//  ToDoListApp
+//
+//  Created by Владимир Федичев on 8/3/25.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension ToDoTask {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<ToDoTask> {
+        return NSFetchRequest<ToDoTask>(entityName: "ToDoTask")
+    }
+
+    @NSManaged public var title: String?
+    @NSManaged public var details: String?
+    @NSManaged public var createdAt: Date?
+    @NSManaged public var isCompleted: Bool
+
+}
+
+extension ToDoTask : Identifiable {
+
+}
